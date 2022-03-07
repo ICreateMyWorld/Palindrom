99b62ed7f4cc4d9d1ef0255f17be45c5d01b9253
ICreateMyWorld <maja.jablonska82@gmail.com>
2022-03-07 22:24:52 +0100

    Dodalam dwie opcje, pierwsza z wielka litera i druga z tekstem

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,26 @@
 def is_palindorme(word):
     return word[::-1]== word
-print(is_palindorme("kajak"))
+print(is_palindorme("Kajak"))
 print(is_palindorme("potop"))
+
+#opcja druga z prosba aby Kajak rowniez zwracal True
+def is_palindorme(word):
+    return word.lower()[::-1]== word.lower()
+print(is_palindorme("Kajak"))
+print(is_palindorme("potop"))
+
+#opcja trzecia - funkcja dla calego zdania
+
+def is_palindrome(tekst):
+    zdanie = tekst(filter(lambda char: char.isalnum(), tekst))
+    return zdanie[::-1]== zdanie
+
+
+
+
+
+
+
+
+
+
